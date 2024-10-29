162ce9d6204edbd10aea9e4f0ee93569208c559d
Devansh1741 <rathoredevansh1741@gmail.com>
2024-10-29 18:22:51 +0530

    InitialCommit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,13 +4,22 @@ import { useUser } from "../context/UserContext";
 function Profile() {
   const { activeUser } = useUser();
   console.log(activeUser);
-  if (!activeUser) return <></>;
+  // if (!activeUser) return <></>;
 
   return (
     <div>
       <h5>TaskRabbit</h5>
-      <h1>{activeUser.userName}</h1>
-      <img src={activeUser.profileUrl} alt="Profile picture" />
+      {activeUser && (
+        <>
+          <h1>{activeUser.userName}</h1>
+          <img
+            src={activeUser.profileUrl}
+            alt="Profile"
+            height={"50px"}
+            width={"auto"}
+          />
+        </>
+      )}
       <br />
       <Link to="/register">Register </Link>
       <Link to="/login">Login </Link>
